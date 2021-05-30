9609281fdb9fa9b59d6146318f0fe7ba4963cb25
thesameh <shahinsameh1000@yahoo.com>
2021-05-30 12:38:06 +0200

    a simple calculator V2 .. feedback improvments

--- 1_calc/calc.c
+++ 1_calc/calc.c
@@ -1,72 +1,79 @@
 // Sameh Shahin ew20b001
+// V2
+
+
 
 #include <stdio.h>
 #include <math.h>
+#include <libc.h>
 
-double excute_operation(double result, char op);
+double excute_operation(double result, char op[]);
 
 int main()
 {
     //  variables
     double final_result;
-    char operation[5];
+    double temp_input;
+    char operation[10];
 
     // read an initial input number once
     scanf("%lf", &final_result);
 
-    /*
-    * loop through the inputs in an (operation/value) pairs
-    * check for the operation at the begining of each loop
-    * read the operation at the end of the loop
-    */
+    // read an initial operation
+    scanf("%s", &operation);
 
-    while (operation[0] != '=')
+    while (strcmp(operation, "=") != 0)
     {
+        // excute the operation based on its type
+        final_result = excute_operation(final_result, operation);
 
-        // 2 - check operation - read second input if needed - excute operation
-        final_result = excute_operation(final_result, operation[0]);
-
-        // 1 - read the operation
-        scanf("%s", operation);
+        // read the next operation
+        scanf("%s", &operation);
     }
 
     printf("Result: %.3f\n", final_result);
     return 0;
 }
 
-// since none of the operations share the first char in the string array we can check for that first char only
 // excute the operation based on its type and read second input if neded
-double excute_operation(double result, char op)
+double excute_operation(double result, char op[])
 {
+    double temp_input;
 
-    double input_1;
-
-    // only read a second input if needed else skip and go to the next (operation/value) pair loop
-    if (op != 's' && op != 'n')
+    // operations with no need to second input
+    if (strcmp(op, "sqrt") == 0)
     {
-        scanf("%lf", &input_1);
-    }
-
-    switch (op)
-    {
-    case 's':
         result = sqrt(result);
-        break;
-    case 'n':
-        result = result * -1;
-        break;
-    case '+':
-        result += input_1;
-        break;
-    case '-':
-        result -= input_1;
-        break;
-    case '*':
-        result = result * input_1;
-        break;
-    case '/':
-        result = result / input_1;
-        break;
     }
+    else if (strcmp(op, "neg") == 0)
+    {
+        result = result * -1;
+    }
+    else
+    {
+        // operations with need to second input
+        scanf("%lf", &temp_input);
+
+        if (strcmp(op, "+") == 0)
+        {
+            result += temp_input;
+        }
+        else if (strcmp(op, "-") == 0)
+        {
+
+            result -= temp_input;
+        }
+        else if (strcmp(op, "*") == 0)
+        {
+
+            result = result * temp_input;
+        }
+        else if (strcmp(op, "/") == 0)
+        {
+
+            result = result / temp_input;
+        }
+    }
+
     return result;
 }
