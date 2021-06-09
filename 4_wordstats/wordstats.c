64e7939c90c474bce5aba9f2cd908c82d79c62b6
thesameh <shahinsameh1000@yahoo.com>
2021-06-09 22:19:27 +0200

    char arrays are already a pointer no need to add & when reading

--- 4_wordstats/wordstats.c
+++ 4_wordstats/wordstats.c
@@ -28,7 +28,7 @@ int main()
 
     word *words_dyn_array = (word *)malloc(initial_size * sizeof(word));
 
-    scanf("%s", &input_word);
+    scanf("%s", input_word);
 
     while (strcmp(input_word, ".") != 0)
     {
@@ -43,7 +43,7 @@ int main()
             // keep track of how many words are added
             words_counter++;
         }
-        scanf("%s", &input_word);
+        scanf("%s", input_word);
     }
 
     // the variable words_counter can be used but just to make sure i'm getting the right data
@@ -56,19 +56,23 @@ int main()
 
 word store_word(int max_word_length, char input_word[max_word_length])
 {
+    /* 
     char *stored_word = (char *)malloc((strlen(input_word) + 1) * sizeof(char));
-
-    //stored_word = input_word; // BIG stupid mistake
 
     for (int s = 0; s < strlen(input_word); s++)
     {
         stored_word[s] = input_word[s];
     }
-    stored_word[strlen(input_word) + 1] = '\0';
+    stored_word[strlen(input_word)] = '\0'; 
+    */
+
+    //* a better way
+    char *stored_word = strdup(input_word);
 
     word stored_word_info = {stored_word, strlen(stored_word)};
     return stored_word_info;
 }
+
 int check_word(int max_word_length, char input_word[], int words_counter, int initial_size, word words_dyn_array[initial_size])
 {
     int add_this_word = 0;
